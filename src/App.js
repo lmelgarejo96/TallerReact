dd7cb8ec0a280d2b377ea78121de0786acb971b3
Usuario de Logon Laboratorio Petit Thouars 385 <LAB-USR-PT385-1406@grupoutp.net>
2020-02-29 13:36:39 -0500

    Primero cambio realizado

--- src/App.js
+++ src/App.js
@@ -1,34 +1,56 @@
-import React, { useState } from 'react';
-import MyComponent from './MyComponet'
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//Render function
+//Hooks
+const App = () => {
+    const [nombre, setNombre] = useState('Luis');
+    const [edad, setEdad] = useState(0);
 
-function App() {
-  const [ count, setCount ] = useState(0)
+    const addEdad = () => {
+        setEdad(edad+1);
+    }
 
-  const addMount = () => {
-    setCount(count+1)
-  } 
+    const removeEdad = () => {
+        setEdad(edad-1);
+    }
 
-  const removeMount = () => {
-    setCount(count-1)
-  } 
+    const addNombre = () => {
+        
+    }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <MyComponent name="Johan"/>
-        <button onClick={addMount}>
-          +
-        </button>
-        <button onClick={removeMount}>
-          -
-        </button>
-        <h1>{count}</h1>
-      </header>
-    </div>
-  );
+    return ( 
+        <div className="App">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>{ nombre }</h1>
+            <div className="buttons">
+                <button onClick={removeEdad}>
+                -
+                </button>
+                <button onClick={addEdad}>
+                +
+                </button>
+                
+            </div>
+            <h4>Edad: {edad}</h4>
+        </div>
+    )
 }
 
 export default App;
+
+
+// class render
+/* class App2 extends Component{
+    constructor() {
+        this.state = {
+            name: 'Luis'
+        }
+    }
+
+    render(){
+        <div>
+            {this.state.name}
+        </div>
+    }
+} */
